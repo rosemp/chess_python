1db5908374b5de346e66985a34d56a7b68831a6d
rosemp <mrose8@gmail.com>
2020-06-14 19:38:44 -0400

    Rearrange board to match Wikipedia article.

--- chess.py
+++ chess.py
@@ -1,18 +1,20 @@
 
 def print_board(current_board):
+    # Should update this to print board with a grid
     print("Current chess board:")
     for i in range(len(current_board)):
         print(current_board[i])
 
 
-board = [["wr", "wn", "wb", "wk", "wq", "wb", "wn", "wr"],
-         ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
-         ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
-         ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
-         ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
-         ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+board = [["br", "bn", "bb", "bq", "bk", "bb", "bn", "br"],
          ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
-         ["br", "bn", "bb", "bk", "bq", "bb", "bn", "br"]]
+         ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+         ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+         ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+         ["  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "],
+         ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+         ["wr", "wn", "wb", "wq", "wk", "wb", "wn", "wr"]]
+         
 player = "w"
 
 print_board(board)
@@ -74,13 +76,13 @@ for i in range(5):
             if move_dist == 2:
                 # White player
                 if player == "w":
-                    if pawn_pos[0] != 1:
+                    if pawn_pos[0] != 6:
                         print("\nThis pawn has already been moved, so you can only move it by one space.")
                         move_dist = 1
                         continue
                 # Black player
                 else:
-                    if pawn_pos[0] != 6:
+                    if pawn_pos[0] != 1:
                         print("\nThis pawn has already been moved, so you can only move it by one space.")
                         move_dist = 1
                         continue
@@ -88,9 +90,9 @@ for i in range(5):
             # Check whether the space is empty
             new_pawn_row = pawn_pos[0]
             if player == "w":
-                new_pawn_row += move_dist
-            else:
                 new_pawn_row -= move_dist
+            else:
+                new_pawn_row += move_dist
             if board[new_pawn_row][pawn_pos[1]] != "  ":
                 print("\nThere is already another piece in your selected spot.")
                 continue
@@ -98,11 +100,11 @@ for i in range(5):
             # For a pawn moving two places, make sure it's not blocked
             if move_dist == 2:
                 if player == "w":
-                    if board[pawn_pos[0] + 1][pawn_pos[1]] != "  ":
+                    if board[pawn_pos[0] - 1][pawn_pos[1]] != "  ":
                         print("Your pawn is blocked!")
                         continue
                 else:
-                    if board[pawn_pos[0] - 1][pawn_pos[1]] != "  ":
+                    if board[pawn_pos[0] + 1][pawn_pos[1]] != "  ":
                         print("Your pawn is blocked!")
                         continue
         
@@ -129,17 +131,15 @@ for i in range(5):
                 print("\nError: Please enter an attack direction of 'left' or 'right'")
                 continue
             if player == "w":
-                attack_pos[0] = pawn_pos[0] + 1
+                attack_pos[0] = pawn_pos[0] - 1
                 if board[attack_pos[0]][attack_pos[1]][0] != "b":
                     print("Error: You can only attack a black piece")
                     continue
-
             else:
-                attack_pos[0] = pawn_pos[0] - 1
+                attack_pos[0] = pawn_pos[0] + 1
                 if board[attack_pos[0]][attack_pos[1]][0] != "w":
                     print("Error: You can only attack a white piece")
                     continue
-
         else:
             print("\nError: You must enter 'forward' or 'attack'.")
             continue
@@ -152,10 +152,10 @@ for i in range(5):
     board[pawn_pos[0]][pawn_pos[1]] = "  "
     if move_type == "forward":
         if player == "w":
-            pawn_pos[0] += move_dist
+            pawn_pos[0] -= move_dist
             board[pawn_pos[0]][pawn_pos[1]] = "wp"
         else:
-            pawn_pos[0] -= move_dist
+            pawn_pos[0] += move_dist
             board[pawn_pos[0]][pawn_pos[1]] = "bp"
     elif move_type == "attack":
         if player == "w":
