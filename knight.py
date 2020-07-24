8bf2d805191034a3354d8fc64c92eb3f4173ef3e
ShravanPY <shravanks2016@gmail.com>
2020-07-23 20:43:39 -0400

    Add code to knight.py that stores valid moves

--- knight.py
+++ knight.py
@@ -6,9 +6,25 @@ from game_state import *
 
 def move_knight(player,piece_pos,move_to):
     # Check that the user enters a valid knight move
-    
+    possible_moves = []
+    possible_moves.append([piece_pos[0] - 1, piece_pos[1] + 2])
+    possible_moves.append([piece_pos[0] - 2, piece_pos[1] + 1])
+    possible_moves.append([piece_pos[0] - 2, piece_pos[1] - 1])
+    possible_moves.append([piece_pos[0] - 1, piece_pos[1] - 2])
+    possible_moves.append([piece_pos[0] + 1, piece_pos[1] - 2])
+    possible_moves.append([piece_pos[0] + 2, piece_pos[1] - 1])
+    possible_moves.append([piece_pos[0] + 2, piece_pos[1] + 1])
+    possible_moves.append([piece_pos[0] + 1, piece_pos[1] + 2])
     # This function returns True if no errors are detected.
+    # Check if move_to is one of these possible moves
     return True
     
+
+
+
+
+
+
+
 
 
